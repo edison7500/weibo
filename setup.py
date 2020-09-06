1b0d91e7a84c75616b222951776b4dd9e425468d
jiaxin <edison7500@gmail.com>
2020-09-07 00:25:31 +0800

    add pre-commit and drop python2 support

--- setup.py
+++ setup.py
@@ -4,24 +4,22 @@
 from setuptools import setup
 
 setup(
-    name='weibo',
-    version='0.2.2',
-    description='Python sina weibo sdk',
-    author='Lx Yu',
-    author_email='i@lxyu.net',
-    py_modules=['weibo', ],
-    url='http://lxyu.github.io/weibo/',
+    name="weibo",
+    version="0.2.2",
+    description="Python sina weibo sdk",
+    author="Lx Yu",
+    author_email="i@lxyu.net",
+    py_modules=["weibo",],
+    url="http://lxyu.github.io/weibo/",
     license="BSD",
-    long_description=open('README.rst').read(),
-    install_requires=[
-        "requests>=2.4.0",
-    ],
+    long_description=open("README.rst").read(),
+    install_requires=["requests>=2.4.0",],
     classifiers=[
         "Topic :: Software Development",
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-    ])
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
