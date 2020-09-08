4534437e5877e0fea5c6c99bb2e972802b7695b8
jiaxin <edison7500@gmail.com>
2020-09-08 20:08:51 +0800

    merge

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="weibo-ng",
-    version="0.1.0",
+    version="0.2.2",
     description="Python sina weibo sdk",
     author="Lx Yu",
     author_email="i@lxyu.net",
